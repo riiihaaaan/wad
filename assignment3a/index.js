fd5852e8d1f40f3659b7cdfacea6ef3322fdb85b
riiihaaaan <riiihaaaan@gmail.com>
2026-05-12 11:43:09 +0530

    dir updated

--- assignment3a/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-app.use(express.static('public'));
-
-app.listen(4000, () => {
-    console.log('Server is running on port 4000');
-});
-
